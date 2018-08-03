c50c9b739491d1abad4c9881cea32e5fc4b7a601
小倉裕一 <j211025ogu@gmail.com>
2018-08-03 11:03:06 +0900

    :sparkles: コメント一覧作成

---
+++ components/CommentList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <CommentListItem
+      v-for="comment in comments"
+      :key="comment.id"
+      :comment="comment"
+    />
+  </div>
+</template>
+
+<script>
+import CommentListItem from '~/components/CommentListItem.vue'
+export default {
+  props: {
+    comments: {
+      type: Array
+    }
+  },
+  components: {
+    CommentListItem
+  },
+  // TODO:: 一旦モックデータを表示APIができたらちゃんとしたデータを表示する
+  created() {
+    this.comments = [
+      { id: 'aaaa', user: { nickname: 'スーパーマリオ' }, message: 'やっふうぅぅぅぅ！！！！', createdAt: '2018-08-03T12:02:01Z' },
+      { id: 'bbbb', user: { nickname: 'スーパーマリオ' }, message: 'やっふうぅぅぅぅ！！！！', createdAt: '2017-08-02T12:02:01Z' },
+      { id: 'cccc', user: { nickname: 'スーパーマリオ' }, message: 'I’m bookmarking this for next year, we went to s pumpkin patch in the North East this year & it was so disappointing, it was just supermarket pumpkins positioned in straw & hay!', createdAt: '2018-05-02T12:02:01Z' }
+    ]
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/CommentListItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="comment-item">
+    <el-row>
+      <el-col
+        class="icon-box"
+        :span="2"
+      >
+        <!-- TODO: ここにユーザーのアイコンを表示する -->
+        <!-- <img /> -->
+      </el-col>
+      <el-col :span="20">
+        <div>
+          <span class="comment-user"> {{ comment.user.nickname }} </span>
+          <span class="comment-date"> {{ commentAt(comment.createdAt) }} </span>
+        </div>
+        <div>
+          <p class="comment-message"> {{ comment.message }} </p>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { someDaysAgo } from '~/assets/js/date-format.js'
+export default {
+  props: {
+    comment: {
+      type: Object
+    }
+  },
+  methods: {
+    commentAt (date) {
+      return someDaysAgo(date)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.comment-item {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.icon-box {
+  height: 50px;
+  width: 50px;
+  background-color: #7b7b7b;
+  margin-right: 10px;
+}
+.comment-user {
+  font-weight: bold;
+  color: #2b2b2b;
+  font-size: 12px;
+}
+.comment-date {
+  font-size: 12px;
+  color: #4b4b4b;
+}
+.comment-message {
+  font-size: 14px;
+}
+</style>
