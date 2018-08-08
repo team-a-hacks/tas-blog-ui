299a86fc410ecf0df434359c7d605a21e414f9da
田代拓也 <tashitaku@tashiro-Mac-mini.local>
2018-08-08 11:07:22 +0900

    :construction: 記事詳細(途中)

---
+++ assets/js/sidebar-contents.js
@@ -0,0 +1,14 @@
+export default {
+	date() {
+		return {
+			sample: [{
+				title: 'article',
+				icon: '',
+				route: {
+					name: 'article',
+					path: '/text'
+				}
+			}]
+		}
+	}
+}
---
+++ pages/article.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="post">
+		<div class="post-contents">
+			<el-row>
+				<el-col span="24" class="top-conter">
+					<img
+						class="photo-style"
+						src="~/static/sample1.png"
+					>
+					<h2 class="page-title">てすとおぉ</h2>
+				</el-col>
+				<el-col span="24">
+					<span class="post-text">
+					</span>
+				</el-col>
+			</el-row>
+		</div>
+		<div class="comments">
+			<CommentForm />
+			<CommentList />
+		</div>
+	</div>
+</template>
+
+<script>
+import CommentList from '~/components/CommentList'
+import CommentForm from '~/components/CommentForm'
+export default {
+	components: {
+		CommentList,
+		CommentForm
+	}
+}
+</script>
+<style lang="scss" scoped>
+.top-conter {
+	position: relative;
+	.photo-style {
+		width: 100%;
+	}
+	.page-title {
+		position: absolute;
+		text-align: center;
+		color: #ffffff;
+		bottom: 0;
+		right: 0;
+		left: 0;
+	}
+}
+</style>
+
